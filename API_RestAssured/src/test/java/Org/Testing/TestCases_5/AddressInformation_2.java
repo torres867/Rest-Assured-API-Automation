93cefac845d5ae1ae29a35fc8e86f6de06416329
torres867 <shashankpandey867@gmail.com>
2022-09-28 02:19:03 +0530

    Address Information for the Complex JSON

---
+++ API_RestAssured/src/test/java/Org/Testing/TestCases_5/AddressInformation_2.java
@@ -0,0 +1,43 @@
+package Org.Testing.TestCases_5;
+
+public class AddressInformation_2 
+{
+	private String House_No;
+	private String Block;
+	private String Place;
+	private String District;
+	private String City;
+	public String getHouse_No() {
+		return House_No;
+	}
+	public void setHouse_No(String house_No) {
+		House_No = house_No;
+	}
+	public String getBlock() {
+		return Block;
+	}
+	public void setBlock(String block) {
+		Block = block;
+	}
+	public String getPlace() {
+		return Place;
+	}
+	public void setPlace(String place) {
+		Place = place;
+	}
+	public String getDistrict() {
+		return District;
+	}
+	public void setDistrict(String district) {
+		District = district;
+	}
+	public String getCity() {
+		return City;
+	}
+	public void setCity(String city) {
+		City = city;
+	}
+	
+	
+
+}
