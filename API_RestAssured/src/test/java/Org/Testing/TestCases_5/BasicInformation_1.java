21d68fa1fbd9aec29dd017c577b275dc2e369fab
torres867 <shashankpandey867@gmail.com>
2022-09-28 02:00:16 +0530

    Basic Information Class for Simple JSON

---
+++ API_RestAssured/src/test/java/Org/Testing/TestCases_5/BasicInformation_1.java
@@ -0,0 +1,37 @@
+package Org.Testing.TestCases_5;
+
+public class BasicInformation_1 
+{
+	private String First_Name;
+	private String Last_Name;
+	private String Designation;
+	private String id;
+	
+	public String getFirst_Name() {
+		return First_Name;
+	}
+	public void setFirst_Name(String first_Name) {
+		First_Name = first_Name;
+	}
+	public String getLast_Name() {
+		return Last_Name;
+	}
+	public void setLast_Name(String last_Name) {
+		Last_Name = last_Name;
+	}
+	public String getDesignation() {
+		return Designation;
+	}
+	public void setDesignation(String designation) {
+		Designation = designation;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
+
+}
