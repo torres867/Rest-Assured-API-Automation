f7541b1f95f95d7db07fc174525904f917013591
torres867 <shashankpandey867@gmail.com>
2022-09-28 02:42:18 +0530

    Basic Information for the in form of Array

---
+++ API_RestAssured/src/test/java/Org/Testing/TestCases_5/BasicInformation_3.java
@@ -0,0 +1,49 @@
+package Org.Testing.TestCases_5;
+
+public class BasicInformation_3 
+{
+	private String First_Name;
+	private String Last_Name;
+	private String Designation;
+	private String id;
+	private AddressInformation_3[] address;
+	private ProjectInformation[] project;
+	public ProjectInformation[] getProject() {
+		return project;
+	}
+	public void setProject(ProjectInformation[] project) {
+		this.project = project;
+	}
+	public AddressInformation_3[] getAddress() {
+		return address;
+	}
+	public void setAddress(AddressInformation_3[] address) {
+		this.address = address;
+	}
+	public String getFirst_Name() {
+		return First_Name;
+	}
+	public void setFirst_Name(String first_Name) {
+		First_Name = first_Name;
+	}
+	public String getLast_Name() {
+		return Last_Name;
+	}
+	public void setLast_Name(String last_Name) {
+		Last_Name = last_Name;
+	}
+	public String getDesignation() {
+		return Designation;
+	}
+	public void setDesignation(String designation) {
+		Designation = designation;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+
+}
