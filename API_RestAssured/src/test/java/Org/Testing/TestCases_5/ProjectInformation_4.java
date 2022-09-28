a1c5bd6b27d7e453dff05a2fcb77bd08301fe672
torres867 <shashankpandey867@gmail.com>
2022-09-29 01:06:07 +0530

    Project Information Class for the in form of Array JSON

---
+++ API_RestAssured/src/test/java/Org/Testing/TestCases_5/ProjectInformation_4.java
@@ -0,0 +1,29 @@
+package Org.Testing.TestCases_5;
+
+public class ProjectInformation_4 
+{
+	private String ProjectName;
+	private String ProjectDomain;
+	private String Role;
+	public String getProjectName() {
+		return ProjectName;
+	}
+	public void setProjectName(String projectName) {
+		ProjectName = projectName;
+	}
+	public String getProjectDomain() {
+		return ProjectDomain;
+	}
+	public void setProjectDomain(String projectDomain) {
+		ProjectDomain = projectDomain;
+	}
+	public String getRole() {
+		return Role;
+	}
+	public void setRole(String role) {
+		Role = role;
+	}
+	
+	
+
+}
