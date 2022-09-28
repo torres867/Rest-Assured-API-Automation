5c123de95199eab09b9a037b8eed3f1af8a9f30b
torres867 <shashankpandey867@gmail.com>
2022-09-29 01:05:39 +0530

    Address Information Class for the in form of Array

---
+++ API_RestAssured/src/test/java/Org/Testing/TestCases_5/AddressInformation_4.java
@@ -0,0 +1,49 @@
+package Org.Testing.TestCases_5;
+
+public class AddressInformation_4 
+{
+	private String HouseNo;
+	private String Block;
+	private String Place;
+	private String District;
+	private String City;
+	private String Type;
+	public String getHouseNo() {
+		return HouseNo;
+	}
+	public void setHouseNo(String houseNo) {
+		HouseNo = houseNo;
+	}
+	public String getBlock() {
+		return Block;
+	}
+	public void setBlock(String block) {
+		Block = block;
+	}
+	public String getPlace() {
+		return Place;
+	}
+	public void setPlace(String place) {
+		Place = place;
+	}
+	public String getDistrict() {
+		return District;
+	}
+	public void setDistrict(String district) {
+		District = district;
+	}
+	public String getCity() {
+		return City;
+	}
+	public void setCity(String city) {
+		City = city;
+	}
+	public String getType() {
+		return Type;
+	}
+	public void setType(String type) {
+		Type = type;
+	}
+	
+
+}
