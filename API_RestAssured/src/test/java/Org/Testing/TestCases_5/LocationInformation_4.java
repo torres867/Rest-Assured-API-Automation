c74fa1ad3ee58d388b5ca683d2c4e2f6ec1b6fad
torres867 <shashankpandey867@gmail.com>
2022-09-29 01:06:27 +0530

    Location Information Class for the in form of Array JSON

---
+++ API_RestAssured/src/test/java/Org/Testing/TestCases_5/LocationInformation_4.java
@@ -0,0 +1,37 @@
+package Org.Testing.TestCases_5;
+
+public class LocationInformation_4 
+{
+	private String JobLocation;
+	private String Building;
+	private String Floor;
+	private String Cubicle;
+	public String getJobLocation() {
+		return JobLocation;
+	}
+	public void setJobLocation(String jobLocation) {
+		JobLocation = jobLocation;
+	}
+	public String getBuilding() {
+		return Building;
+	}
+	public void setBuilding(String building) {
+		Building = building;
+	}
+	public String getFloor() {
+		return Floor;
+	}
+	public void setFloor(String floor) {
+		Floor = floor;
+	}
+	public String getCubicle() {
+		return Cubicle;
+	}
+	public void setCubicle(String cubicle) {
+		Cubicle = cubicle;
+	}
+	
+	
+	
+
+}
